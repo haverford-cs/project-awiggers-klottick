086e45746b0ee1e246729bc7da7dc4f6f8b772b5
Lottick Kadan <klottick@haverford.edu>
2019-12-12 13:48:07 -0500

    making main code nicer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from process import *
-import fc_nn
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
@@ -14,25 +13,31 @@ def main():
     y = data[:,-1]
     X = data
     X = np.delete(X, -1, axis=1)
-
-
+    epochs = 1
+    n,p = X.shape
     train_dset = tf.data.Dataset.from_tensor_slices((X, y))
     train_dset = train_dset.shuffle(X.shape[0])
 
-    inputs = tf.keras.Input(shape = (92,), name = "input")
+    model = get_compiled_model(p, keras.optimizers.Adam())
+    history = train(X, y, epochs, model)
+    print(history.history)
+
+def train(X, y, epochs, model, validation_split = .1):
+        history = model.fit(X, y, batch_size=1, epochs=epochs, validation_split = 0.1)
+        return history
+def get_uncompiled_model(p):
+    inputs = tf.keras.Input(shape = (p,), name = "input")
     x = Dense(10, tf.nn.relu)(inputs)
     outputs = Dense(3, tf.nn.softmax)(x)
     model = tf.keras.Model(inputs = inputs, outputs = outputs)
-
-    model.compile(optimizer=keras.optimizers.Adam(),  # Optimizer
+    return model
+def get_compiled_model(p,optimizer):
+    model = get_uncompiled_model(p)
+    model.compile(optimizer=optimizer,  # Optimizer
               # Loss function to minimize
               loss=keras.losses.SparseCategoricalCrossentropy(),
               # List of metrics to monitor
               metrics=[keras.metrics.SparseCategoricalAccuracy()])
-
-    history = model.fit(X, y, batch_size=1, epochs=10, validation_split = 0.1)
-
-    print(history.history)
-
+    return model
 if __name__ == "__main__" :
     main()
