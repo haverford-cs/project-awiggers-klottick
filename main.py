98a4fff9bd49682f599a0a87e69979cc918a946d
awiggers <awiggers@haverford.edu>
2019-12-17 20:47:34 -0500

    added confusion matrices

--- main.py
+++ main.py
@@ -18,11 +18,10 @@ source_file = "spreadspoke_scores.csv"
 output_file = "results.txt"
 
 def main():
-    year = 0
+    year = 2005
     epochs = 5
     batch_size = 50
     single_test(year, epochs, batch_size)
-    #y = energyusage.evaluate(multi_test, epochs, batch_size, printToScreen = False, pdf = True)
     #multi_test(epochs, batch_size)
 
 
@@ -55,7 +54,7 @@ def multi_test(epochs, batch_size):
 
         #create a 'count' number of models
         for i in range(count):
-            history,results = runModel(year, epochs, batch_size)
+            history,results,confusionMatrix = runModel(year, epochs, batch_size)
             print(str(year) + " " + str(i))
             s = str(year) + " test " + str(i) + ": "
             s += str(results[0]) + ", " + str(results[1])
@@ -89,11 +88,12 @@ def single_test(year, epochs, batch_size):
     using given epochs and batch_size. Then print results
     for the model.
     """
-    history,results = runModel(year, epochs, batch_size)
+    history,results,confusionMatrix = runModel(year, epochs, batch_size)
 
     #print(history.history)
     print_epoch_scores(history)
     print(results)
+    print(confusionMatrix)
     print(get_bookie_score(source_file))
 
 
@@ -151,7 +151,13 @@ def runModel(startYear, epochs, batch_size):
     #test model
     results = test(X_test, y_test, model)
 
-    return history,results
+    confusionMatrix = generateConfusionMatrix(X_test, y_test, model)
+
+    keras.backend.clear_session()
+
+
+
+    return history,results,confusionMatrix
 
 
 
@@ -197,6 +203,19 @@ def get_compiled_model(p,optimizer):
               metrics=[keras.metrics.SparseCategoricalAccuracy()])
 
     return model
+
+def generateConfusionMatrix(X_test, y_test, model):
+    outcomes = model.predict(X_test)
+    matrix = np.zeros((2,2))
+    n,p = X_test.shape
+    for i in range(n):
+        prediction = 0
+        if outcomes[i][1] > outcomes[i][0]:
+            prediction = 1
+        matrix[y_test[i]][prediction] +=1
+    return matrix
+
+
 
 def print_epoch_scores(history):
     """
